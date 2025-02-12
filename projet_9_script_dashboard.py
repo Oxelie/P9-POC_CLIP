fbc5472089b1f3975ac926cd956931d7bbab4234
Oxelie <stephanieduhem.simplon@gmail.com>
2025-02-13 00:18:11 +0100

    clean commentaires

--- projet_9_script_dashboard.py
+++ projet_9_script_dashboard.py
@@ -69,14 +69,7 @@ if article_index is not None:
     
     st.write(f"**Description de l'article sélectionné:**")
     st.write(f"{description}")
-    # st.markdown('</div>', unsafe_allow_html=True)
-    # st.markdown(
-    # f"""
-    # <div class="rounded-frame">
-    #     <p>{description}</p>
-    # </div>
-    # """,
-    # unsafe_allow_html=True)
+ 
 
     # Bouton pour afficher la prédiction
     st.write(" ")
@@ -107,95 +100,4 @@ if article_index is not None:
         st.write(" ")
         st.write(" ")
         
-              
-# # Afficher le contenu du notebook en HTML
-# st.write(" ")
-# st.write(" ")
-# st.write(" ")
-# st.write("### Notebook d'analyses et de comparaison des modélisations")
-# with open("projet_9_test_CLIP.html", "r", encoding="utf-8") as f:
-#     html_content = f.read()
-# components.html(html_content, height=800, scrolling=True)
-
-
-
-# # Prétraiter les images et les descriptions
-# images = [preprocess(Image.open(image_path)) for image_path in data['reshaped_image_path']] # colonne à modfier ?
-# max_length = 77
-# model.context_length = max_length
-# texts = clip.tokenize(data['description'], context_length=model.context_length, truncate=True).squeeze(0)
-
-# # Empiler les images en un seul tenseur
-# image_input = torch.stack(images).to(device)
-
-# # Encoder les images et les descriptions
-# with torch.no_grad():
-#     image_features = model.encode_image(image_input)
-#     text_features = model.encode_text(texts)
-    
-# # Normaliser les caractéristiques
-# image_features /= image_features.norm(dim=-1, keepdim=True)
-# text_features /= text_features.norm(dim=-1, keepdim=True)
-
-# # Combiner les caractéristiques des images et des textes
-# combined_features = torch.cat((image_features, text_features), dim=1).cpu().numpy()
-
-# # # Fonction pour faire des prédictions
-# # def predict(image_path, description):
-# #     image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-# #     text = clip.tokenize([description]).to(device)
-
-# #     with torch.no_grad():
-# #         image_features = model.encode_image(image)
-# #         text_features = model.encode_text(text)
-
-# #     image_features /= image_features.norm(dim=-1, keepdim=True)
-# #     text_features /= text_features.norm(dim=-1, keepdim=True)
-
-# #     similarity = (image_features @ text_features.T).item()
-# #     return similarity
-
-# # Entraîner le classificateur
-# classifier = LogisticRegression(max_iter=1000)
-# classifier.fit(X_train, y_train)
-
-# # Fonction pour faire des prédictions
-# def predict(image_path, description):
-#     start_time = time.time()
-#     image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-#     text = clip.tokenize([description], context_length=model.context_length, truncate=True).to(device)
-
-#     with torch.no_grad():
-#         image_features = model.encode_image(image)
-#         text_features = model.encode_text(text)
-
-#     image_features /= image_features.norm(dim=-1, keepdim=True)
-#     text_features /= text_features.norm(dim=-1, keepdim=True)
-
-#     combined_features = torch.cat((image_features, text_features), dim=1).cpu().numpy()
-#     prediction = classifier.predict(combined_features)
-#     end_time = time.time()
-#     duration = end_time - start_time
-#     return prediction[0], duration
-
-# # Interface utilisateur avec Streamlit
-# st.title("Dashboard de Prédiction CLIP")
-
-# # Sélection de l'image
-# image_path = st.selectbox("Sélectionnez une image", data['reshaped_image_path'].tolist())
-
-# # Saisie de la description
-# description = st.text_input("Entrez une description")
-
-# # Bouton pour faire la prédiction
-# if st.button("Prédire"):
-#     if image_path and description:
-#         prediction, duration = predict(image_path, description)
-#         st.image(Image.open(image_path), caption="Image sélectionnée")
-#         st.write(f"Description : {description}")
-#         st.write(f"Catégorie prédite : {prediction}")
-#         minutes, seconds = divmod(duration, 60)
-#         duration_str = f"{int(minutes)} min {int(seconds)} sec"
-#         st.write(f"Temps de calcul : {duration_str}")
-#     else:
-#         st.write("Veuillez sélectionner une image et entrer une description.")
+  
